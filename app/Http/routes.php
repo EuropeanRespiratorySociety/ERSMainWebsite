56554489f2f856a58bb9afb3de12911226e1391a
EuropeanRespiratorySociety <spouyt@E6440-002.ersnet.org>
2016-02-05 13:36:44 +0100

    Improved rooting system

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,138 +15,10 @@ Route::get('/', function () {
     return view('pages.blank');
 });
 
-Route::group(['prefix' => 'pages'], function () {
-	Route::get('blank', function () {
-	    return view('pages.blank');
-	});
-
-	Route::get('blank-header', function () {
-	    return view('pages.blank-header');
-	});
-
-	Route::get('blank-aside', function () {
-	    return view('pages.blank-aside');
-	});
-
-	Route::get('calendar', function () {
-	    return view('pages.calendar');
-	});
-
-	Route::get('stats', function () {
-	    return view('pages.stats');
-	});
-});
-
+//Special Routes
 Route::group(['prefix' => 'errors'], function () {
 	Route::get('{errorCode}', function ($errorCode) {
 	    return view('errors.error',['error' => $errorCode]);
-	});
-});
-
-
-Route::group(['prefix' => 'forms'], function () {
-	Route::get('elements', function () {
-	    return view('forms.elements');
-	});
-
-	Route::get('masks', function () {
-	    return view('forms.masks');
-	});
-
-	Route::get('upload', function () {
-	    return view('forms.upload');
-	});
-
-	Route::get('validation', function () {
-	    return view('forms.validation');
-	});
-
-	Route::get('wizard', function () {
-	    return view('forms.wizard');
-	});
-});
-
-Route::group(['prefix' => 'profile'], function () {
-	Route::get('edit', function () {
-	    return view('profile.edit');
-	});
-	
-	Route::get('member', function () {
-	    return view('profile.member');
-	});
-	
-	Route::get('non-member', function () {
-	    return view('profile.non-member');
-	});
-
-	Route::get('variations', function () {
-	    return view('profile.variations');
-	});
-});
-
-Route::group(['prefix' => 'tables'], function () {
-	Route::get('datatables', function () {
-	    return view('tables.datatables');
-	});
-	
-	Route::get('general', function () {
-	    return view('tables.general');
-	});
-});
-
-Route::group(['prefix' => 'ui'], function () {
-	Route::get('alerts', function () {
-	    return view('ui.alerts');
-	});
-	
-	Route::get('buttons', function () {
-	    return view('ui.buttons');
-	});
-	
-	Route::get('general', function () {
-	    return view('ui.general');
-	});
-	
-	Route::get('grid', function () {
-	    return view('ui.grid');
-	});
-	
-	Route::get('icons', function () {
-	    return view('ui.icons');
-	});
-	
-	Route::get('modals', function () {
-	    return view('ui.modals');
-	});
-	
-	Route::get('nestable-lists', function () {
-	    return view('ui.nestable-lists');
-	});
-	
-	Route::get('notifications', function () {
-	    return view('ui.notifications');
-	});
-	
-	Route::get('panels', function () {
-	    return view('ui.panels');
-	});
-	
-	Route::get('tabs-accordions', function () {
-	    return view('ui.tabs-accordions');
-	});
-});
-
-Route::group(['prefix' => 'special'], function () {
-	Route::get('login', function () {
-	    return view('special.login');
-	});
-
-	Route::get('sign-up', function () {
-	    return view('special.sign-up');
-	});
-
-	Route::get('forgot', function () {
-	    return view('special.forgot');
 	});
 });
 
@@ -154,6 +26,13 @@ Route::get('/cache-flush', function () {
 	ResponseCache::flush();
 	return "ok";
 });
+
+//Genera routing Vies fiels need to follow the correct partern
+Route::get('/{param1}/{param2}', function ($param1,$param2) {
+    return view($param1.'.'.$param2);
+});
+
+
 
 
 /*
