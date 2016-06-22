c968f84c3f82d43473204c2954c8fe08670591eb
idealley <samuelpouyt@gmail.com>
2016-06-22 10:13:31 +0200

    fixed typo

--- resources/views/partials/category-items.blade.php
+++ resources/views/partials/category-items.blade.php
@@ -16,7 +16,7 @@
             @if(isset($item['type']))
             <span class="label {{ $item['typeColor'] }}">{{ $item['type'] }}</span>
             @endif
-            @if(isset($item['flags']) && $item['flags']['color'] !== 'info'd)
+            @if(isset($item['flags']) && $item['flags']['color'] !== 'info')
             <span class="label {{ 'label-'.$item['flags']['color'] }}">{{ $item['flags']['text'] }}</span>
             @endif
             @if($item['fullyBooked'])
