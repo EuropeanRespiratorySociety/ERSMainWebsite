4b62c63e9c7996afa6de19df690660c07610c9e4
rtanabe1027 <rika.tanabe-hirsig@ersnet.org>
2016-08-30 10:02:48 +0200

    Meta Nav Logo

--- resources/views/sidebar/left-variant.blade.php
+++ resources/views/sidebar/left-variant.blade.php
@@ -1,7 +1,12 @@
 
     <div class="ers-left-sidebar">
     <div class="navbar-header" style="width:100%;">
-            <a href="/" class="navbar-brand"></a>
+            <!--a href="/" class="navbar-brand"></a-->
+
+            <a href="/" class="navbar-brand">
+              <i class="icon-ers icon" style="font-size:50px; color:#d0043c;"></i>
+            </a>
+
     </div>
       <div class="content">
         <div class="ers-logo"></a></div>
