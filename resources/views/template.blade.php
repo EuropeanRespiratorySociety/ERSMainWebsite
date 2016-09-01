7370eba6229e63fba13cb988332b52e4938a89ed
idealley <samuelpouyt@gmail.com>
2016-09-01 09:53:24 +0200

    Google Tag manager

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -37,6 +37,15 @@
 
   </head>
   <body id="ersMainWebsite">
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5Z6KP9"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5Z6KP9');</script>
+<!-- End Google Tag Manager -->
 
     <div class="ers-wrapper ers-fixed-sidebar ers-white-header">
 
@@ -70,15 +79,6 @@
     @yield('scripts')
 
     <!--<script src="/js/sw-registration.js" type="text/javascript"></script>-->
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-      ga('create', 'UA-77738133-2', 'auto');
-      ga('require', 'linkid');
-      ga('send', 'pageview');
-</script>
   </body>
 </html>
