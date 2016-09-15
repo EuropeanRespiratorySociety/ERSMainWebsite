003d7f3749e52c679f3bc0ab225747bfaaedcc54
idealley <samuelpouyt@gmail.com>
2016-09-15 18:12:06 +0200

    Added search on google results

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -26,6 +26,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         @yield('meta')
         @yield('structured-data')
+    <script type="application/ld+json">
+    {
+      "@context": "http://schema.org",
+      "@type": "WebSite",
+      "url": "https://www.ersnet.org/",
+      "potentialAction": {
+        "@type": "SearchAction",
+        "target": "https://www.ersnet.org/search?query={search_term_string}",
+        "query-input": "required name=search_term_string"
+      }
+    }
+</script>    
     <!--[if lt IE 9]>     
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
