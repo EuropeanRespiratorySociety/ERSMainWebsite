535d18c268a2afd61677c8d7461a124a53d1b6c2
idealley <samuelpouyt@gmail.com>
2016-09-16 14:45:44 +0200

    Fixed typo

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -90,10 +90,37 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
     @yield('scripts')
     <script type="text/javascript">
-        console.log(navigator.userAgent.indexOf("MSIE"));
-        if($.browser.msie){
-            $('.ers-white-header .ers-top-header .navbar-collapse .navbar-nav > li.open.horizontal > .dropdown-menu > li').css('min-width', '50%');
-        }
+    // Get IE or Edge browser version
+    var version = detectIE();
+    console.log(version);
+
+    if (version !== false) {
+      $('.ers-white-header .ers-top-header .navbar-collapse .navbar-nav > li.open.horizontal > .dropdown-menu > li').css('min-width', '50%');
+    }
+    function detectIE() {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf('MSIE ');
+  if (msie > 0) {
+    // IE 10 or older => return version number
+    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+  }
+
+  var trident = ua.indexOf('Trident/');
+  if (trident > 0) {
+    // IE 11 => return version number
+    var rv = ua.indexOf('rv:');
+    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+  }
+
+  var edge = ua.indexOf('Edge/');
+  if (edge > 0) {
+    // Edge (IE 12+) => return version number
+    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+  }
+
+  // other browser
+  return false;
+}
     </script>
 
     {{--<script src="/js/sw-registration.js" type="text/javascript"></script>--}}
