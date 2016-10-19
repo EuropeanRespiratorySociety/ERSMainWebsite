2b0b590c060df42f87f2444231ae036e3499e233
idealley <samuelpouyt@gmail.com>
2016-10-19 22:14:44 +0200

    Small fix for fellowships

--- resources/views/partials/modal-items.blade.php
+++ resources/views/partials/modal-items.blade.php
@@ -1,6 +1,6 @@
 @foreach ($items as $item)
 <!-- Start Modal -->
-    <div id="@if(isset($item->slug)){{$item->slug}}@endif" tabindex="-1" role="dialog" class="modal fade" style="display: none;">
+    <div id="@if($item->slug){{$item->slug}}@endif" tabindex="-1" role="dialog" class="modal fade" style="display: none;">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
@@ -15,19 +15,20 @@
             @endif
           </div>
           <div class="modal-body">
-            <div class="text-left">
-            @if($item->sponsors)
-            <div class="">
-                <p class="">Sponsor: {{$item->sponsors->text}}
-                @if($item->sponsors->image)
-                    <img style="height:25px;" src="{{ $item->sponsors->image}}" class="img-responsive">
-                @endif    
-                </p>
-            </div>
-            @endif  
+            <div class="text-left"> 
               @if($item->lead){!! $item->lead !!}@endif
               @if($item->body){!! $item->body !!}@endif
             </div>
+          </div>
+          <div class="modal-footer">
+            @if($item->sponsors)
+            <div class="">
+                <h3 class="">Sponsor: {{$item->sponsors->text}}</h3>
+                @if($item->sponsors->image)
+                    <img class="display:inline;" style="height:60px;" src="{{ $item->sponsors->image}}" class="img-responsive">
+                @endif    
+            </div>
+            @endif 
           </div>
         </div>
       </div>
