51721586b3e5038472b4dfc6a5b3b502393e3c28
idealley <samuelpouyt@gmail.com>
2016-11-10 14:41:47 +0100

    Timeline component

--- resources/views/congress-and-events/timeline.blade.php
+++ resources/views/congress-and-events/timeline.blade.php
@@ -66,58 +66,12 @@
 @stop()  
 
 @section('scripts')  
+    <script src="https://bootstrap.ersnet.org/js/app-timeline.js" type="text/javascript"></script>
+
     <script type="text/javascript">
-        (function() {
-
-        'use strict';
-
-        // define variables
-        var items = document.querySelectorAll(".timeline li.event");
-
-        function setHeight(){
-            var els = document.querySelectorAll(".event");
-            for (var i = 0; i < els.length; i++) {
-                var height = els[i].children[0].offsetHeight;
-                els[i].style.height = height + 20 + "px";
-            }
-        }
-
-        function setWidth(){
-            var width = document.querySelector('.timeline').offsetWidth - 100;
-            var els = document.querySelectorAll(".card-container");
-            for (var i = 0; i < els.length; i++) {
-                els[i].style.width = width + "px";
-            }
-        }
-
-        // check if an element is in viewport
-        // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-        function isElementInViewport(el) {
-            var rect = el.getBoundingClientRect();
-            return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-            );
-        }
-
-        function callbackFunc() {
-            setHeight();
-            setWidth();
-            for (var i = 0; i < items.length; i++) {
-            if (isElementInViewport(items[i])) {
-                items[i].classList.add("in-view");
-            }
-            }
-        }
-
-        // listen for events
-        window.addEventListener("load", callbackFunc);
-        window.addEventListener("resize", callbackFunc);
-        window.addEventListener("scroll", callbackFunc);
-
-    })();
+      $(document).ready(function(){
+        App.timeline();        
+      });
     </script>
 
 @stop()
