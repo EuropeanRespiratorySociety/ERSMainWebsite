cf97e8f7ddf628f7af6fe88c137f71ccbaf686eb
idealley <samuelpouyt@gmail.com>
2016-11-14 10:37:25 +0100

    Fixed card action

--- resources/views/elements/card/action.blade.php
+++ resources/views/elements/card/action.blade.php
@@ -2,13 +2,11 @@
     @if($item->registerButton->link)
     <a href="{{$item->registerButton->link}}" target="new_blank"  class="btn btn-register">{{$item->registerButton->text ? $item->registerButton->text : "register"}}</a>
     @endif
-    @if($item->contentType != "event_calendar")
-        @if($item->uri)
+        @if($item->uri !="false" && $item->uri)
             <a href="{{url($item->uri)}}"  class="btn btn-register">more</a>
-        @elseif($item->url)
+        @elseif($item->url !="false" && $item->url)
             <a href="{{url($item->url)}}"  class="btn btn-register">more</a>
-        @elseif($item->slug)
+        @elseif($item->slug && $item->contentType != "event_calendar")
             <a href="{{Request::path().'/'.$item->slug}}"  class="btn btn-register">more</a>
         @endif
-    @endif
 </div>
