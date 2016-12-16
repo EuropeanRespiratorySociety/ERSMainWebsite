6f4c23e8d18e8a370a3bcd5c907c3b088b45234c
idealley <samuelpouyt@gmail.com>
2016-12-16 09:23:10 +0100

    Fix news breaking by allowing a tags

--- server/app.js
+++ server/app.js
@@ -25,30 +25,43 @@ server.report(function(callback) {
 
     // provide some debug info
     console.log("");
-    console.log("Node Version: " + process.version);
-    console.log("Server Mode: " + process.env.CLOUDCMS_APPSERVER_MODE);
-    console.log("Server Base Path: " + process.env.CLOUDCMS_APPSERVER_BASE_PATH);
-    console.log("Gitana Scheme: " + process.env.GITANA_PROXY_SCHEME);
-    console.log("Gitana Host: " + process.env.GITANA_PROXY_HOST);
-    console.log("Gitana Port: " + process.env.GITANA_PROXY_PORT);
-    console.log("CPU Count: " + cpuCount);
-    console.log("Virtual Hosting domain: " + process.env.CLOUDCMS_DOMAIN);
-    console.log("Store Configuration: " + process.env.CLOUDCMS_STORE_CONFIGURATION);
-    console.log("Broadcast Provider: " + process.env.CLOUDCMS_BROADCAST_TYPE);
-    console.log("Cache Provider: " + process.env.CLOUDCMS_CACHE_TYPE);
-    console.log("Temp Directory: " + process.env.CLOUDCMS_TEMPDIR_PATH);
-    console.log("LaunchPad Mode: " + process.env.CLOUDCMS_LAUNCHPAD_SETUP);
+    // console.log("Node Version: " + process.version);
+    // console.log("Server Mode: " + process.env.CLOUDCMS_APPSERVER_MODE);
+    // console.log("Server Base Path: " + process.env.CLOUDCMS_APPSERVER_BASE_PATH);
+    // console.log("Gitana Scheme: " + process.env.GITANA_PROXY_SCHEME);
+    // console.log("Gitana Host: " + process.env.GITANA_PROXY_HOST);
+    // console.log("Gitana Port: " + process.env.GITANA_PROXY_PORT);
+    // console.log("CPU Count: " + cpuCount);
+    // console.log("Virtual Hosting domain: " + process.env.CLOUDCMS_DOMAIN);
+    // console.log("Store Configuration: " + process.env.CLOUDCMS_STORE_CONFIGURATION);
+    // console.log("Broadcast Provider: " + process.env.CLOUDCMS_BROADCAST_TYPE);
+    // console.log("Cache Provider: " + process.env.CLOUDCMS_CACHE_TYPE);
+    // console.log("Temp Directory: " + process.env.CLOUDCMS_TEMPDIR_PATH);
+    // console.log("LaunchPad Mode: " + process.env.CLOUDCMS_LAUNCHPAD_SETUP);
 
     console.log("AWS Key: " + process.env.SQS_ACCESS_KEY);
     console.log("AWS Secret: " + process.env.SQS_SECRET_KEY);
-    console.log("Node environment: " + process.env.NODE_ENV);
+    // console.log("Node environment: " + process.env.NODE_ENV);
 
-    console.log("");
-    console.log("Web Server: http://localhost:" + process.env.PORT);
-    console.log("");
+    // console.log("");
+    // console.log("Web Server: http://localhost:" + process.env.PORT);
+    // console.log("");
+
+    console.log("Node Version: " + process.version);
+    console.log("Temp Directory: " + process.env.CLOUDCMS_TEMPDIR_PATH);
+
+
+    if (process.env.CLOUDCMS_VIRTUAL_HOST) {
+        console.log("Mode: Single Virtual Host (" + process.env.CLOUDCMS_VIRTUAL_HOST + ")");
+        } else if (process.env.CLOUDCMS_VIRTUAL_HOST_DOMAIN) {
+        console.log("Mode: Multiple Virtual Hosts (*." + process.env.CLOUDCMS_VIRTUAL_HOST_DOMAIN + ")");
+        } else {
+        console.log("Mode: Standalone");
+        }
+        console.log("Node process running on port: " + process.env.PORT);
+        console.log("");
 
     callback();
-
 });
 
 // start the server
@@ -76,12 +89,20 @@ var config = {
     },
     "perf": {
         "enabled": true,
-        "paths": [{
+        "paths": [
+            {
             "regex": "/static/.*",
             "cache": {
                 "seconds": 300
+                }
+            },
+            {
+            "regex": "/preview/.*",
+            "cache": {
+                "seconds": 300
+                }
             }
-        }]
+        ]
     }    
 
 };
