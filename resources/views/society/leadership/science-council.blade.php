ba2a10beccf4524c97949f387b178d07989b5e79
idealley <samuelpouyt@gmail.com>
2016-12-19 15:17:52 +0100

    Url change

--- resources/views/society/leadership/science-council.blade.php
+++ resources/views/society/leadership/science-council.blade.php
@@ -8,7 +8,7 @@
 	        </div>
 	        <div class="col-md-8 center-block lead">This committee comprises of the members listed below and the <a href="http://old.ersnet.org/about-us/leadership/scientific-committee/category/assembly-heads-2.html">Heads of Scientific Assemblies</a><br>
 			
-			Download the brochure - <a target="_blank" href="http://www.ersnet.org/images/stories/scientific/Brochure_scientific_activities_2014-15.pdf">Scientific Activities 2015<br></a>
+			Download the brochure - <a target="_blank" href="https://ers.app.box.com/s/7s16sz4py6f08wsdvis9ct4rlw0d1gki">Scientific Activities</a><br>
 			
 			Consult the <a href="{{url('the-society/who-we-are/science-portal')}}">Science Council Portal</a>
 
