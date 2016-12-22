160c8c4abca91fe28a9d3c7050dd06f8ef12d766
idealley <samuelpouyt@gmail.com>
2016-12-22 14:33:14 +0100

    added some default styles

--- resources/views/sidebar/left-variant-api.blade.php
+++ resources/views/sidebar/left-variant-api.blade.php
@@ -1,4 +1,13 @@
-<div class="ers-left-sidebar" style="position:fixed;z-index:1032;">
+<div class="ers-left-sidebar" 
+    style="
+        position:fixed;
+        z-index:1032;
+        position: fixed;
+        background-color: #fff;
+        width: 100px;
+        height: 100%;
+        left: 0;
+    ">
     <div class="navbar-header" style="width:100%;">
         <a href="https://www.ersnet.org" class="navbar-brand">
             <i class="icon-ers icon" style="font-size:50px; color:#d0043c;"></i>
