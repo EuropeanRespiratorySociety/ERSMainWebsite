3175ab52772d40a62672925bae4de23a290889a5
idealley <samuelpouyt@gmail.com>
2017-01-11 07:40:15 +0100

    Swapped people

--- resources/views/society/leadership/advocacy-council.blade.php
+++ resources/views/society/leadership/advocacy-council.blade.php
@@ -63,8 +63,8 @@
 		            </p>
 	        	</div>
 	        	<div class="col-md-4 xs-mb-15">
-					<img src="images/people/leadership/Ben_Nemery.jpg" class="img-circle">
-		            <p class="photo_caption"><strong>{!! Html::mailto('', 'Ben Nemery') !!}</strong>
+					<img src="images/people/leadership/constantine-vardavas.jpg" class="img-circle">
+		            <p class="photo_caption"><strong>Constantine Vardavas</strong>
 		            Director of Scientific Relations with the EU<br>
 		            </p>
 	        	</div>
