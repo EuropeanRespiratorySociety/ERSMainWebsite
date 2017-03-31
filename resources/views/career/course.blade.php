d894fb7dcb567fbc6e5b56fe30ba729a04c7db90
tasanoers <tamaki.asano@ersnet.org>
2017-03-31 13:46:50 +0000

    #51 - Show all sponsor

--- resources/views/career/course.blade.php
+++ resources/views/career/course.blade.php
@@ -52,25 +52,25 @@
         @endif
       </div>
 
+      @foreach ($item->sponsors as $sponsor)
       <div class="course-sponsor text-left">
-        @if($item->sponsors->text)
+        @if($sponsor->text)
           <div class="course-sponsor-wrapper">
-            @if($item->sponsors->image)
+            @if($sponsor->image)
               <div class="course-sponsor-image col-md-4 col-xs-4 pull-left">
-                <p style="background-image: url('{{ $item->sponsors->image }}'); background-repeat: no-repeat; background-size:100%; width: 100%; height: 100%; background-position: right center; background-size: contain;"></p>
+                <p style="background-image: url('{{ $sponsor->image }}'); background-repeat: no-repeat; background-size:100%; width: 100%; height: 100%; background-position: right center; background-size: contain;"></p>
              </div> 
             @endif
-            @if($item->sponsors->text)
+            @if($sponsor->text)
               <div class="course-sponsor-right  col-md-8 col-xs-8 pull-right">
-                <h4 class="text-left">{{$item->sponsors->text}}</h4>
+                <h4 class="text-left">{{$sponsor->text}}</h4>
               </div>
             @endif
           </div>
         @endif
       </div>
-
+    @endforeach
     </div>
-
 
     <div class="col-md-6 lighter-grey-bg ers-course-info">
       <div class="header">
