71b8af672e0290653883600ba02eeede177e946b
idealley <samuelpouyt@gmail.com>
2017-05-22 18:30:21 +0200

    Changed courses  document links to box.com

--- resources/views/professional/course-footer.blade.php
+++ resources/views/professional/course-footer.blade.php
@@ -3,10 +3,10 @@
 		<h3>Are you interested in organising an ERS course?</h3>
 		
 		<div class="col-md-8 center-block">
-			Please download and fill one of the following forms: <a href="documents/education/ERS_External_courses_application_form.doc">External course</a>, <a href="documents/education/ERS_Skills_based_course_application_form.doc">Skills based course</a>, <a href="documents/education/ERS_Online_course_application_form.DOC">Online course.</a>
+			Please download and fill one of the following forms: <a href="https://ers.box.com/v/external-courses-application">External course</a>, <a href="https://ers.box.com/v/skills-course-application">Skills based course</a>, <a href="https://ers.box.com/v/online-course-application">Online course.</a>
 			<br><br>
 			
-			If you would like ERS endorsement for your event please complete the following <a href="documents/education/Endorsement_Application_Form.doc">form</a> please see these <a href="documents/education/Guidelines_for_meeting_endorsement_by_ERS_2015.pdf">guidelines</a> for further information. Forms can be returned either by email {!! Html::mailto('education@ersnet.org', 'education@ersnet.org') !!} or fax +41 21 213 0100.
+			If you would like ERS endorsement for your event please complete the following <a href="https://ers.box.com/v/endorsement-form">form</a> please see these <a href="https://ers.box.com/v/guidelines-meeting-endorsement">guidelines</a> for further information. Forms can be returned either by email {!! Html::mailto('education@ersnet.org', 'education@ersnet.org') !!} or fax +41 21 213 0100.
 
 		</div>
 		<p>
