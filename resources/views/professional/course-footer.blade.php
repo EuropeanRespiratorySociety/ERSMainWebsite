51e3b998da262fc1a23054932b7ba0a3f28bbd43
idealley <samuelpouyt@gmail.com>
2017-06-22 17:35:45 +0200

    Changed course footer with new text

--- resources/views/professional/course-footer.blade.php
+++ resources/views/professional/course-footer.blade.php
@@ -1,10 +1,8 @@
 <div class="row"> 
 	<div class="col-md-9 courses-footer">   
-		<h3>Are you interested in organising an ERS course?</h3>
+		<h3>Are you interested in organising an event endorsed by the ERS?</h3>
 		
 		<div class="col-md-8 center-block">
-			Please download and fill one of the following forms: <a href="https://ers.box.com/v/external-courses-application">External course</a>, <a href="https://ers.box.com/v/skills-course-application">Skills based course</a>, <a href="https://ers.box.com/v/online-course-application">Online course.</a>
-			<br><br>
 			
 			If you would like ERS endorsement for your event please complete the following <a href="https://ers.box.com/v/endorsement-form">form</a> please see these <a href="https://ers.box.com/v/guidelines-meeting-endorsement">guidelines</a> for further information. Forms can be returned either by email {!! Html::mailto('education@ersnet.org', 'education@ersnet.org') !!} or fax +41 21 213 0100.
 
