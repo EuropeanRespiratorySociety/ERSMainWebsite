ca98a37bcc2b0926f676c21e40bb56bd69b9fdf2
idealley <samuelpouyt@gmail.com>
2018-02-06 00:07:48 +0100

    added dtm tracking

--- app/Http/Middleware/SlidingMetanavigation.php
+++ app/Http/Middleware/SlidingMetanavigation.php
@@ -21,27 +21,30 @@ class SlidingMetanavigation
 
             //the maximum depth is 4
 
-            $menu->add('MyERS', 'https://my.ersnet.org')->prepend('<i class="icon-my-ers icon"></i><span>')->append('</span>'); 
-            $menu->add('Publications', url("#publications"))->prepend('<i class="icon-publications icon"></i><span>')->append('</span>'); 
+            $menu->add('MyERS', 'https://my.ersnet.org')->prepend('<i class="icon-my-ers icon"></i><span>')->append('</span>')->link->attr(array('class' => 'ga-metanav')); 
+            $menu->add('Publications', url("#publications"))
+                ->prepend('<i class="icon-publications icon"></i><span>')
+                ->append('</span>')
+                ->link->attr(array('class' => 'ga-metanav')); 
             $menu->add('e-learning', 'http://www.ers-education.org/home.aspx')
                     ->prepend('<i class="icon-e-learn icon"></i><span>')->append('</span>')
                     ->link->attr(array('target' => '_blank'));
                     
             $menu->add('ERS Vision', 'http://www.ersvision.org/')
                     ->prepend('<i class="icon-vision icon"></i><span>')->append('</span>')
-                    ->link->attr(array('target' => '_blank')); 
+                    ->link->attr(array('target' => '_blank', 'class' => 'ga-metanav'));
             $menu->add('Healthy Lungs For Life', 'http://www.europeanlung.org/en/projects-and-research/projects/healthy-lungs-for-life/home/')
                     ->prepend('<i class="icon-hlfl icon" style="font-size:39px; margin-bottom:10px"></i><span>')->append('</span>')
-                    ->link->attr(array('target' => '_blank')); 
+                    ->link->attr(array('target' => '_blank', 'class' => 'ga-metanav'));
             $menu->add('European Lung Foundation', 'http://www.europeanlung.org/en/')
                     ->prepend('<i class="icon-elf icon" style="font-size:40px"></i><span>')->append('</span>')
-                    ->link->attr(array('target' => '_blank')); 
+                    ->link->attr(array('target' => '_blank', 'class' => 'ga-metanav'));
             $menu->add('TB Consilium', 'https://www.tbconsilium.org/')
                     ->prepend('<div class="tb_consilium_nav clearfix" style="margin-top:10px;"><i class="icon-ers icon pull-left"></i><i class="icon-who icon pull-right who-icon"></i></div><span>')->append('</span>')
-                    ->link->attr(array('target' => '_blank')); 
+                    ->link->attr(array('target' => '_blank', 'class' => 'ga-metanav'));
             $menu->add('FIRS', 'https://www.firsnet.org/')
                     ->prepend('<div class="firs_nav clearfix" style="margin-top:10px;"></div><span>')->append('<div style="font-size:12px"><i>United for Lung Health</i></div></span>')
-                    ->link->attr(array('target' => '_blank')); 
+                    ->link->attr(array('target' => '_blank', 'class' => 'ga-metanav'));
               
 
         });
