32bf383c33b6341daf6a8815fa2f1dcb8c5ae561
tasanoers <tamaki.asano@ersnet.org>
2018-02-12 14:26:45 +0100

    #56 - Correct the conditions on the card for the Spirometry on Calendar

--- resources/views/elements/card/action.blade.php
+++ resources/views/elements/card/action.blade.php
@@ -8,13 +8,13 @@
         class="btn btn-register">{{$item->registerButton->text ? $item->registerButton->text : "register"}}
     </a>
     @endif
-        @if($item->uri !="false" && $item->uri)
-            <a href="{{url($item->uri)}}"  class="btn btn-register">more</a>
-        @elseif($item->url !="false" && $item->url)
-            <a href="{{url($item->url)}}"  class="btn btn-register">more</a>
-        @elseif($item->slug && $item->contentType != "event_calendar")
-            <a href="{{Request::path().'/'.$item->slug}}"  class="btn btn-register">more</a>
-        @elseif($item->type = "Spirometry Programme")
+        @if($item->type = "Spirometry Programme")
             <a href="/professional-development/spirometry-training-programme"  class="btn btn-register">more</a>
+        @elseif($item->uri !="false" && $item->uri && $item->type != "Spirometry Programme")
+            <a href="{{url($item->uri)}}"  class="btn btn-register">more</a>
+        @elseif($item->url !="false" && $item->url && $item->type != "Spirometry Programme")
+            <a href="{{url($item->url)}}"  class="btn btn-register">more</a>
+        @elseif($item->slug && $item->contentType != "event_calendar" && $item->type != "Spirometry Programme")
+            <a href="{{Request::path().'/'.$item->slug}}"  class="btn btn-register">more</a>
         @endif
 </div>
--- resources/views/elements/card/content.blade.php
+++ resources/views/elements/card/content.blade.php
@@ -8,17 +8,15 @@
     <h3 class="title">
     @if(Request::path() !== '/') 
       @if(strpos($item->registerButton->link, '@'))
-          @if($item->type = "Spirometry Programme")
+          @if($item->registerButton->link && $item->type = "Spirometry Programme")
             <a href="/professional-development/spirometry-training-programme">{{ $item->title }}</a>
           @else
             <a href="{{'mailto:'.$item->registerButton->link}}">{{ $item->title }}</a>
           @endif
-      @elseif($item->registerButton->link && $item->type = "Spirometry Programme")
-         <a href="/professional-development/spirometry-training-programme">{{ $item->title }}</a>
       @elseif($item->registerButton->link 
               && ($item->ersEndorsedEvent
                   || $item->nonErsCalendarItem 
-                  || $item->ersDeadline 
+                  || $item->ersDeadline
                   || $item->category->title == "Events Calendar"
                   || $item->contentType == "event_calendar")
               )
