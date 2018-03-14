db53521a5b786819c0a49c27fa311f26f144d242
tasanoers <tamaki.asano@ersnet.org>
2018-03-14 15:20:16 +0100

    Put buck the director on Couse page

--- resources/views/professional/director.blade.php
+++ resources/views/professional/director.blade.php
@@ -1,15 +1,13 @@
 <div class="courses-director medium-grey-bg col-sm-12 {!! $class !!}">
     <div class="row">
        <div class="col-md-9 col-md-offset-1 xs-mb-15">
-            <img src="images/people/leadership/Stefano_Aliberti.jpg" class="img-circle" style="margin-top:20px;">
-            <p class="photo_caption"><strong>Stefano Aliberti</strong>Curriculum Development Director<br></p>
+            <img src="../images/people/leadership/Gilbert_Massard.png" class="img-circle" style="margin-top:20px;">
+            <p class="photo_caption"><strong>Gilbert Massard</strong>Educational Programmes Director<br></p>
         </div>
     </div>
     <div class="row">
         <div class="col-md-12">   
-            <p>Associate Professor and Consultant
-                Department of Pathophysiology and Transplantation, University of Milan
-                Internal Medicine Department, Respiratory Unit and Cystic Fibrosis Adult Center</p>
+            <p>Professor Gilbert Massard, Thoracic Surgeon Consultant, is currently the Director of the Lung Transplantation Department at the University Hospital of Strasbourg.</p>
         </div>
     </div>
 </div>
