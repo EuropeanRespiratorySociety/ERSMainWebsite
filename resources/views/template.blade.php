b312f94226fb64074a282753de05b7bd6436c966
idealley <samuelpouyt@gmail.com>
2018-04-24 17:28:18 +0200

    Added missing elments from ers CDN

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -3,23 +3,23 @@
 <html lang="en">
   <head>
     <base href="/">
-    <link rel="apple-touch-icon" sizes="57x57" href="https://d3egf9svnop2xg.cloudfront.net/images/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="https://d3egf9svnop2xg.cloudfront.net/images/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="https://d3egf9svnop2xg.cloudfront.net/images/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="https://d3egf9svnop2xg.cloudfront.net/images/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="https://d3egf9svnop2xg.cloudfront.net/images/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="https://d3egf9svnop2xg.cloudfront.net/images/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="https://d3egf9svnop2xg.cloudfront.net/images/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="https://d3egf9svnop2xg.cloudfront.net/images/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="https://d3egf9svnop2xg.cloudfront.net/images/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="https://d3egf9svnop2xg.cloudfront.net/images/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="https://d3egf9svnop2xg.cloudfront.net/images/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="https://d3egf9svnop2xg.cloudfront.net/images/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="https://d3egf9svnop2xg.cloudfront.net/images/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="https://cdn.ersnet.org/images/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="https://cdn.ersnet.org/images/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="https://cdn.ersnet.org/images/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="https://cdn.ersnet.org/images/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="https://cdn.ersnet.org/images/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="https://cdn.ersnet.org/images/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="https://cdn.ersnet.org/images/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="https://cdn.ersnet.org/images/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://cdn.ersnet.org/images/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="https://cdn.ersnet.org/images/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://cdn.ersnet.org/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="https://cdn.ersnet.org/images/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://cdn.ersnet.org/images/favicon-16x16.png">
     
-    <link rel="manifest" href="https://d3egf9svnop2xg.cloudfront.net/images/manifest.json">
+    <link rel="manifest" href="https://cdn.ersnet.org/images/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="https://d3egf9svnop2xg.cloudfront.net/images/ms-icon-144x144.png">
+    <meta name="msapplication-TileImage" content="https://cdn.ersnet.org/images/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
 
     <meta charset="utf-8">
@@ -45,7 +45,7 @@
     <![endif]-->
 
     {{-- Cloud Front is the ERS CDN --}}
-    <link rel="stylesheet" href="https://d3egf9svnop2xg.cloudfront.net/css/all.css" type="text/css"/>
+    <link rel="stylesheet" href="https://cdn.ersnet.org/css/all.css" type="text/css"/>
     {{-- <link rel="stylesheet" href="https://bootstrap.ersnet.org/css/all.css" type="text/css"/> --}}
     {{--<link rel="stylesheet" href="http://erstemplate.app/css/all.css" type="text/css"/>--}}
 
@@ -85,10 +85,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     </div>
     
     {{-- Cloud Front is the ERS CDN --}}
-    <script src="https://d3egf9svnop2xg.cloudfront.net/js/jquery.min.js" type="text/javascript"></script>
+    <script src="https://cdn.ersnet.org/js/jquery.min.js" type="text/javascript"></script>
     {{-- <script src="https://bootstrap.ersnet.org/js/jquery.min.js" type="text/javascript"></script> --}}
     {{--<script src="http://erstemplate.app/js/jquery.min.js" type="text/javascript"></script>--}}
-    <script src="https://d3egf9svnop2xg.cloudfront.net/js/all.js" type="text/javascript"></script>
+    <script src="https://cdn.ersnet.org/js/all.js" type="text/javascript"></script>
     {{-- <script src="https://bootstrap.ersnet.org/js/all.js" type="text/javascript"></script> --}}
     {{--<script src="http://erstemplate.app/js/all.js" type="text/javascript"></script>--}}
 
