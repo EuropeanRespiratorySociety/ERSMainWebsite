2647a904d44b5f06a982b54ce58b83b1267ee840
tasanoers <tamaki.asano@ersnet.org>
2018-10-08 15:32:31 +0200

    Modify Early Career Members Committee page

--- resources/views/society/leadership/early-career-members-committee.blade.php
+++ resources/views/society/leadership/early-career-members-committee.blade.php
@@ -4,13 +4,46 @@
 	<div class="col-md-10">
 		<div class="main-content">
 	        <div class="page-head">
-	        <h2 class="">Early Career Members Committee</h2>
+	        <h2>Early Career Members Committee</h2>
 	        </div>
-	        <div class="col-md-8 center-block lead">The Early Career Members Committee (ECMC) represents all ERS members under 40. The committee ensures that early career members are supported in their career and professional development, and that their interests are represented across the Society. It also promotes the highest standards of evidence-based respiratory education in Europe.<br><br>
-	        Early career members are welcome to join meetings held at the Lung Science Conference and the ERS International Congress to help shape the work of the Society and find out more about activities they can get involved with.
-			</div>
-
-
+	        <div class="col-md-8 center-block lead">The Early Career Member Council (ECMC) is a group of ERS members below 40 years of age representing the interests of their fellow Early Career Members (EMCs) from the assemblies.
+            <h3 class="text-left" style="margin-top: 40px;">ECMC mission and vision statement</h3>
+            <div class="text-left">
+               <p>
+                Our vision statement can be summarised by: “In seed time learn, in harvest teach, in winter enjoy" [William Blake]. We envision that ECMs will be nourished as seeds through educational activities and scientific support.
+                Some will flourish and should be taught how to contribute to the society.<br />
+                In time, the ERS will enjoy the benefits of a well-trained generation of researchers and clinicians who can lead the society. This vision can be translated as: “ECMs are the audience of today and the leaders of tomorrow".
+              </p>
+              <p>
+                Our <strong>mission</strong> is to improve the training of all ECMs and to ensure the continued support for ECMs who flourish within the society. The mission translates to the following objectives, per role that ECMs can take:
+              </p>
+              <h4 style="margin-top: 40px;"><i>“In seed time learn” – ECM as audience</i></h4>
+              <ol>
+                  <li>Provide support to ECMs for abstract and fellowship application submissions.</li>
+                  <li>Align the educational activities of the ERS with the needs of ECMs.</li>
+                  <li>Provide a clear training program for respiratory physicians and subspecialties.</li>
+                  <li>Establish workshops aimed at improving soft skills.</li>
+                  <li>Communicate the burden of respiratory diseases directly to ECMs.</li>
+                  <li>Improve the awareness of the importance of societal challenges such as smoking policy and air pollution and involve patients in messaging.</li>
+              </ol>
+              <h4 style="margin-top: 20px;"><i>“In harvest teach” – ECM as contributor</i></h4>
+              <ol>
+                  <li>Generate an open and unbiased selection process for the involvement of ECMs for ERS activities.</li>
+                  <li>Increase the involvement of ECMs in the scientific conference activities through the chairing of sessions and reviewing of abstracts.</li>
+                  <li>Increase the involvement of ECMs to the guideline working groups.</li>
+                  <li>Involve ECMs in the innovation and improvement of educational activities.</li>
+                  <li>Improve peer-to-peer learning via publications in the journals of the ERS.</li>
+              </ol>
+              <h4 style="margin-top: 20px;"><i>“In winter enjoy” – ECM as leader</i></h4>
+              <ol>
+                <li>Establish and maintain a democratic selection process for ECMC representatives and the election of the chair of the ECMC.</li>
+                <li>Contribute to the scientific program of the annual conference with session proposals addressing issues relevant for ECMs.</li>
+                <li>Provide an ECM perspective in the education council, science council, publication committee, advocacy council and executive committee.</li>
+                <li>Support ECMs in their application for leadership roles.</li>
+                <li>Nudge the long-term strategy of the ERS in favor of ECMs.</li>
+                <li>Improve the online strategy of the ERS through a user-friendly platform meeting the needs for ECMs.</li>
+            </div>
+          </div>
 			<div class="row leadership-people" style="padding-top: 40px;">
 	        	<div class="col-md-4 xs-mb-15">
 					<img src="https://cdn.ersnet.org/images/people/leadership/Nicolas-Kahn.jpg" class="img-circle">
