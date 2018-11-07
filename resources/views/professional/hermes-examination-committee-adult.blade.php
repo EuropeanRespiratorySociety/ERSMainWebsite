64c8e4521466d64194ebe64ae633e81bf3ad31fb
tasanoers <tamaki.asano@ersnet.org>
2018-11-07 16:20:34 +0100

    Modify Hermes Committee page

--- resources/views/professional/hermes-examination-committee-adult.blade.php
+++ resources/views/professional/hermes-examination-committee-adult.blade.php
@@ -43,38 +43,7 @@
                         <p>- Zürich, Switzerland</p>
                     </div>
                 </div>
-                <div class="col-md-6 xs-mb-15">
-                    <h4 style="margin-bottom:30px;"><strong>Committee Member</strong></h4>
-                    <img src="https://cdn.ersnet.org/images/people/examination-committee/Christine-Ruegg.png" class="img-circle">
-                    <p class="photo_caption"><strong>Dr Christine Rüegg</strong></p>
-                    <div style="margin: 0 auto;width:70%;" class="text-left photo_caption">
-                      <p style="text-indent: -0.7em;padding-left:0.7em;margin-bottom:0px;">
-                        - Clinic of Pulmonary Medicine and Internal Medicine</p>
-                      <p style="margin-bottom:0px;">- Hospital Uster</p>
-                      <p>- Uster, Switzerland</p>
-                    </div>
-                </div>
-                <div class="col-md-6 xs-mb-15">
-                  <h4 style="margin-bottom:30px;"><strong>Committee Member</strong></h4>
-                  <img src="https://cdn.ersnet.org/images/people/examination-committee/Jayesh-M-Bhatt.png" class="img-circle">
-                  <p class="photo_caption"><strong>Dr Jayesh M Bhatt</strong></p>
-                  <div style="margin: 0 auto;width:70%;" class="text-left photo_caption">
-                    <p style="text-indent: -0.7em;padding-left:0.7em;margin-bottom:0px;">
-                      - Consultant Respiratory Paediatrician</p>
-                    <p style="text-indent: -0.7em;padding-left:0.7em;margin-bottom:0px;">- Nottingham University Hospitals NHS Trust</p>
-                    <p>- Nottingham, United Kingdom</p>
-                  </div>
-              </div>
-              <div class="col-md-6 xs-mb-15">
-                <h4 style="margin-bottom:30px;"><strong>Committee Member</strong></h4>
-                <img src="https://cdn.ersnet.org/images/people/examination-committee/Raffaella-Nenna.png" class="img-circle">
-                <p class="photo_caption"><strong>Dr Raffaella Nenna</strong></p>
-                <div style="margin: 0 auto;width:70%;" class="text-left photo_caption">
-                  <p style="text-indent: -0.7em;padding-left:0.7em;margin-bottom:0px;">
-                    - Researcher "Sapienza" University of Rome</p>
-                  <p style="text-indent: -0.7em;padding-left:0.7em;margin-bottom:40px;">- Rome, Italy</p>
-                </div>
-            </div>
+
             {{-- <div class="col-md-6 xs-mb-15">
                     <h4 style="margin-bottom:30px;"><strong>Committee Member</strong></h4>
                     <img src="https://cdn.ersnet.org/images/people/examination-committee/Thomas-Brack.png" class="img-circle">
