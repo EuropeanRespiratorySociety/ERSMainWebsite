8124179defb3d46b69572e8172b81f7db06c1727
tasanoers <tamaki.asano@ersnet.org>
2019-02-08 16:53:55 +0100

    Modify National page

--- resources/views/professional/national.blade.php
+++ resources/views/professional/national.blade.php
@@ -53,11 +53,7 @@
 
 
         @if($item->body){!! $item->body !!}@endif
-        @if($item->category)
-          @if($item->category->title == "Respiratory Worldwide" )
-            <p><i>Respiratory Worldwide aims to stimulate communication and interaction between national and regional respiratory societies and ERS. These articles have not been (peer) reviewed and are not official ERS content.</i></p>
-          @endif
-        @endif
+
       </div>
       @if($item->comments == true)
       <hr>
