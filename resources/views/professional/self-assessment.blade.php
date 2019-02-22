6b7708d39166e9dbe0560bc2e0ca4d02f123bf09
tasanoers <tamaki.asano@ersnet.org>
2019-02-22 15:40:58 +0100

    Change api adress on self-assessment page

--- resources/views/professional/self-assessment.blade.php
+++ resources/views/professional/self-assessment.blade.php
@@ -120,7 +120,7 @@
         //     cache: 60, //This will cache requests for 60 seconds
         //     cachableMethods: ["GET"] //This defines what method types can be cached (this is already set by default)
         // });
-        var client = new $.RestClient('http://localhost:3030/');
+        var client = new $.RestClient('https://api.ersnet.org/');
  
         client.add('calendar');
         client.calendar.read({type:'selfAssessment'}).done(function (data){
