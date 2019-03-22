18bfb33c3a1c9a5f6597ed6761da178ad7a58b0c
tasanoers <tamaki.asano@ersnet.org>
2019-03-22 14:32:20 +0100

    Add a new functionality for live streaming

--- resources/views/research/research-seminar.blade.php
+++ resources/views/research/research-seminar.blade.php
@@ -43,6 +43,12 @@
     <div class="col-md-6 lighter-grey-bg ers-research-seminar-info">
       <div class="header">
         <div class="clearfix notification">
+          @if($item->feeList->liveStreaming === 0)
+            <p style="text-align:left;color:#d0043c;">
+              <i style="font-size:26px;font-weight:bold;position:relative;top:5px;" class="s7-video"></i>
+              <span>Live streaming will be available</span>
+            </p>
+          @endif
           @if($item->flags->text && $item->flags->color !== 'info')
           <p class="pull-right {{$item->flags->color }}">{{ $item->flags->text }}</p>
           @endif
