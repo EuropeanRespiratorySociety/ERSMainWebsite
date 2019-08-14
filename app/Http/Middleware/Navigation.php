e51eee483bec4a685bf065d219b90e478948e7ff
tasanoers <tamaki.asano@ersnet.org>
2019-08-14 13:35:00 +0200

    Change the route and htaccess for LSC

--- app/Http/Middleware/Navigation.php
+++ app/Http/Middleware/Navigation.php
@@ -124,7 +124,7 @@ class Navigation
                 //$menu->congressAndEvents->add('Paris 2018','congress-and-events/ers-2018');
                 $menu->congressAndEvents->add('The Lung Science Conference','congress-and-events/metabolic-alterations-in-lung-ageing-and-disease')
                 ->link->attr(array('class' => 'ga-nav-congress-and-events'));
-                    $menu->theLungScienceConference->add('Estoril, your host city','congress-and-events/the-lung-science-conference/estoril-your-host-city')->link->attr(array('style' => 'display:none'));
+                    $menu->theLungScienceConference->add('Estoril, your host city','congress-and-events/metabolic-alterations-in-lung-ageing-and-disease/estoril-your-host-city')->link->attr(array('style' => 'display:none'));
                     $menu->theLungScienceConference->add('Previous Conferences','http://old.ersnet.org/previous-conferences.html')
                                             ->link->attr(array('target' => '_blank','style' => 'display:none'));
                 $menu->congressAndEvents->add('Sleep and Breathing Conference','https://sleepandbreathing.org')
