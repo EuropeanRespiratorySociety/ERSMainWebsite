bd9778b78d2a0ca4ad3c10030f9f12efb431c63d
tasanoers <tamaki.asano@ersnet.org>
2019-08-30 14:11:10 +0200

    Add a tub on CPD desease page

--- resources/views/professional/cpd.blade.php
+++ resources/views/professional/cpd.blade.php
@@ -122,6 +122,38 @@
                 </div>
             </div> 
             <div class="main-content">
+
+                <div class="row">
+                    <div class="col-sm-12">
+                      <div class="tab-container">
+                        <ul class="nav nav-tabs">
+                          <li class=""><a href="#home" data-toggle="tab" aria-expanded="false">Pages</a></li>
+                          <li class=""><a href="#profile" data-toggle="tab" aria-expanded="false">Profile</a></li>
+                          <li class="active"><a href="#messages" data-toggle="tab" aria-expanded="true">Messages</a></li>
+                        </ul>
+                        <div class="tab-content">
+                          <div id="home" class="tab-pane cont">
+                            <p> Consectetur adipisicing elit. Ad aperiam dolore veniam mollitia consectetur aut. Cumque sunt consequatur, officiis voluptatum quas atque magnam animi eaque facere cupiditate quos ad totam saepe porro nostrum tenetur. Assumenda esse quidem, sed vel dolore quisquam fuga culpa non, ducimus, impedit fugiat vero similique recusandae?</p>
+                            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam dolore veniam mollitia consectetur aut. Cumque sunt consequatur, officiis voluptatum quas atque magnam animi eaque facere cupiditate quos ad totam saepe porro nostrum tenetur. Assumenda esse quidem, sed vel dolore quisquam fuga culpa non, ducimus, impedit fugiat vero similique recusandae?</p>
+                          </div>
+                          <div id="profile" class="tab-pane cont">
+                            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima praesentium laudantium ipsa, enim maxime placeat, dolores quos sequi nisi iste velit perspiciatis rerum eveniet voluptate laboriosam perferendis ipsum. Expedita, maiores.</p>
+                            <p> Consectetur adipisicing elit. Minima praesentium laudantium ipsa, enim maxime placeat, dolores quos sequi nisi iste velit perspiciatis rerum eveniet voluptate laboriosam perferendis ipsum. Expedita, maiores.</p>
+                          </div>
+                          <div id="messages" class="tab-pane active">
+                            <p>Consectetur adipisicing elit. Ipsam ut praesentium, voluptate quidem necessitatibus quam nam officia soluta aperiam, recusandae.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos facilis laboriosam, vitae ipsum tenetur atque vel repellendus culpa reiciendis velit quas, unde soluta quidem voluptas ipsam, rerum fuga placeat rem error voluptate eligendi modi. Delectus, iure sit impedit? Facere provident expedita itaque, magni, quas assumenda numquam eum! Sequi deserunt, rerum.</p><a href="#">Read more</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+        
+
+
+
+
+
                 @if($item->programme)
                <div class="row">
                  <div class="col-sm-2 col-md-2 col-xs-12 pull-left" style="margin: 10px 0 20px;">
