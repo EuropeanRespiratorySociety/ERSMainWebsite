86cddc57886c2cd56c1509b1cbf05c61a99154a6
tasanoers <tamaki.asano@ersnet.org>
2019-09-12 08:55:43 +0200

    Change the shape of the image in card of Modale

--- resources/views/elements/modal/recommender.blade.php
+++ resources/views/elements/modal/recommender.blade.php
@@ -5,12 +5,12 @@
           <button type="button " data-dismiss="modal" aria-hidden="true" class="close"><i class="icon s7-close"></i></button>
           <h3 class="modal-title">Test md-recommender</h3>
           <div class="card card-event" >
-            <div style="display: flex; flex-direction: row; padding: 10px;">
-              <div><p>ERS course</p></div>
+            <div style="display: flex; flex-direction: row; padding: 10px 15px 5px;">
+              <div>ERS course</div>
               <div style="flex-grow: 1 !important; -webkit-box-flex:1 !important;"></div>
               <div>fully booked</div>
             </div>
-            <div style="height: 200px;  display: flex; flex-direction: row; ">
+            <div style="display: flex; flex-direction: row; ">
                 <div class="card-content text-left" style="flex-basis: 70%;margin-top: 10px; ">
                   <h3 class="title">
                   <a href="professional-development/courses/calendar-sheets-in-malignant-pleural-diseases">Calendar sheets in malignant pleural diseases</a>
@@ -18,14 +18,14 @@
                   <p class="date" style="padding-bottom: 3px;"><span class="icon s7-map-marker"></span>Online</p><p class="date"><span class="icon s7-date"></span>3-24 March, 2020</p>Pleural malignancies represent common thoracic oncological challenges for clinicians in daily practice, be it making the correct diagnosis or...
                 </div>
                 <div style="flex-basis: 30%; height: auto; position: relative;" >
-                  <div style=" position: absolute; top:15px; right:15px ; bottom: 60px ; left: 5px; background-size: cover; background-position: center; transition: transform .2s; background-size:cover;background-repeat: no-repeat;background-image: url('https://cdn.ersnet.org/preview?node=b5472a94145dc85c715b&amp;name=img500&amp;size=500&amp;v=1350701:32118af8135260cfc850');">
+                  <div style="border-radius: 4px; position: absolute; top:15px; right:15px ; bottom: 60px ; left: 5px; background-size: cover; background-position: center; transition: transform .2s; background-size:cover;background-repeat: no-repeat;background-image: url('https://cdn.ersnet.org/preview?node=b5472a94145dc85c715b&amp;name=img500&amp;size=500&amp;v=1350701:32118af8135260cfc850');">
                     <i>image</i>
                   </div>
                 </div>
             </div>
             <div ><a href="professional-development/courses/calendar-sheets-in-malignant-pleural-diseases" class="btn btn-register">more</a></div>
 
-          </div>
+          </div><!-- End card -->
 
         <div class="modal-body">
         </div>
