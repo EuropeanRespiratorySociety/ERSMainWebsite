0ce42874c9b5761389e6058399dea51bd6054892
tasanoers <tamaki.asano@ersnet.org>
2019-09-17 11:37:38 +0200

    Add a some tipo of cards to recommender modal

--- resources/views/elements/modal/recommender.blade.php
+++ resources/views/elements/modal/recommender.blade.php
@@ -4,28 +4,105 @@
         <div class="modal-header">
           <button type="button " data-dismiss="modal" aria-hidden="true" class="close"><i class="icon s7-close"></i></button>
           <h3 class="modal-title">Test md-recommender</h3>
-          <div class="card card-event" >
+
+
+          <div class="card card-event" style="font-family: DinPro,sans-serif;">
             <div style="display: flex; flex-direction: row; padding: 10px 15px 5px;">
-              <div>ERS course</div>
+              <div>ERS COURSE</div>
               <div style="flex-grow: 1 !important; -webkit-box-flex:1 !important;"></div>
-              <div>fully booked</div>
+              <div style="color:#cf003d">fully booked</div>
             </div>
-            <div style="display: flex; flex-direction: row; ">
-                <div class="card-content text-left" style="flex-basis: 70%;margin-top: 10px; ">
-                  <h3 class="title">
+            <div style="padding-bottom: 32px; ">
+                <article class="card-content text-left" style="flex-basis: 70%;margin-top: 10px;padding-right: 132px; ">
+                  <h3 class="title" style="margin-bottom:4px;">
                   <a href="professional-development/courses/calendar-sheets-in-malignant-pleural-diseases">Calendar sheets in malignant pleural diseases</a>
                   </h3>
-                  <p class="date" style="padding-bottom: 3px;"><span class="icon s7-map-marker"></span>Online</p><p class="date"><span class="icon s7-date"></span>3-24 March, 2020</p>Pleural malignancies represent common thoracic oncological challenges for clinicians in daily practice, be it making the correct diagnosis or...
-                </div>
-                <div style="flex-basis: 30%; height: auto; position: relative;" >
-                  <div style="border-radius: 4px; position: absolute; top:15px; right:15px ; bottom: 60px ; left: 5px; background-size: cover; background-position: center; transition: transform .2s; background-size:cover;background-repeat: no-repeat;background-image: url('https://cdn.ersnet.org/preview?node=b5472a94145dc85c715b&amp;name=img500&amp;size=500&amp;v=1350701:32118af8135260cfc850');">
-                    <i>image</i>
-                  </div>
-                </div>
-            </div>
-            <div ><a href="professional-development/courses/calendar-sheets-in-malignant-pleural-diseases" class="btn btn-register">more</a></div>
+                  <p class="date" style="padding-bottom: 3px;"><span class="icon s7-map-marker"></span>&nbsp;Online</p>
+                  <p class="date"><span class="icon s7-date"></span>&nbsp;3-24 March, 2020</p>
 
+                  <figure style="position: absolute; top:40px; right:15px; bottom:auto; left: auto; z-index:1; width:100px; height:100px; overflow: hidden; border-radius: 4px;">
+                      <img style="position: absolute; height: auto; width: 200px; top: 50%; left: 50%; transform: translate(-50%,-50%);" src="https://cdn.ersnet.org/preview?node=b5472a94145dc85c715b&amp;name=img500&amp;size=500&amp;v=1350701:32118af8135260cfc850"/>
+                  </figure>
+                </article>
+            </div>
+            <div class="card-action clearfix" style="position: absolute; right:0;bottom: 0;"> 
+              <a href="professional-development/courses/calendar-sheets-in-malignant-pleural-diseases" class="btn btn-register">more</a>
+            </div>
           </div><!-- End card -->
+
+          <div class="card card-event" style="font-family: DinPro,sans-serif;">
+              <div style="display: flex; flex-direction: row; padding: 10px 15px 5px;">
+                <div>ERS COURSE</div>
+                <div style="flex-grow: 1 !important; -webkit-box-flex:1 !important;"></div>
+                <div style="color:#cf003d">fully booked</div>
+              </div>
+              <div style="padding-bottom: 32px; ">
+                  <article class="card-content text-left" style="flex-basis: 70%;margin-top: 10px;padding-right: 132px; ">
+                    <h3 class="title" style="margin-bottom:4px;">
+                    <a href="professional-development/courses/calendar-sheets-in-malignant-pleural-diseases">Calendar sheets in malignant pleural diseases</a>
+                    </h3>
+                    <p class="date" style="padding-bottom: 3px;"><span class="icon s7-map-marker"></span>&nbsp;Online</p>
+                    <p class="date"><span class="icon s7-date"></span>&nbsp;3-24 March, 2020</p>
+  
+                    <figure style="position: absolute; top:40px; right:15px; bottom:auto; left: auto; z-index:1; width:100px; height:100px; overflow: hidden; border-radius: 4px;">
+                        <img style="position: absolute; height: auto; width: 200px; top: 50%; left: 50%; transform: translate(-50%,-50%);" src="https://cdn.ersnet.org/preview/node/o:9cc3223ea6f4e2ea3967?name=image500&size=500&v=1338737:beab3a8edf44dcc64899"/>
+                    </figure>
+                  </article>
+              </div>
+              <div class="card-action clearfix" style="position: absolute; right:0;bottom: 0;"> 
+                <a href="professional-development/courses/calendar-sheets-in-malignant-pleural-diseases" class="btn btn-register">more</a>
+              </div>
+          </div><!-- End card -->
+
+          <div class="card card-event" style="font-family: DinPro,sans-serif;">
+            <div style="display: flex; flex-direction: row; padding: 10px 15px 5px;">
+              <div>ERS Webinar</div>
+              <div style="flex-grow: 1 !important; -webkit-box-flex:1 !important;"></div>
+              {{-- <div style="color:#cf003d">fully booked</div> --}}
+            </div>
+            <div style="padding-bottom: 32px; ">
+                <article class="card-content text-left" style="flex-basis: 70%;margin-top: 10px;padding-right: 132px; ">
+                  <h3 class="title" style="margin-bottom:4px;">
+                  <a href="professional-development/courses/calendar-sheets-in-malignant-pleural-diseases">EARLY DETECTION OF LUNG CANCER – THE NELSON TRIAL RESULTS IN REAL-LIFE PRACTICE</a>
+                  </h3>
+                  {{-- <p class="date" style="padding-bottom: 3px;"><span class="icon s7-map-marker"></span>&nbsp;Online</p> --}}
+                  <p class="date"><span class="icon s7-date"></span>&nbsp;19 September, 2019 - 18:00 - 19:00 CET</p>
+
+                  <figure style="position: absolute; top:40px; right:15px; bottom:auto; left: auto; z-index:1; width:100px; height:100px; overflow: hidden; border-radius: 4px;">
+                      <img style="position: absolute; height: auto; width: 200px; top: 50%; left: 50%; transform: translate(-50%,-50%);" src="https://cdn.ersnet.org/images/course/webinar-image500x333.jpg"/>
+                  </figure>
+                </article>
+            </div>
+            <div class="card-action clearfix" style="position: absolute; right:0;bottom: 0;"> 
+              <a href="professional-development/courses/calendar-sheets-in-malignant-pleural-diseases" class="btn btn-register">register</a><a href="professional-development/courses/calendar-sheets-in-malignant-pleural-diseases" class="btn btn-register">more</a>
+            </div>
+        </div><!-- End card -->
+
+
+        <div class="card card-event" style="font-family: DinPro,sans-serif;">
+          <div style="display: flex; flex-direction: row; padding: 10px 15px 5px;">
+            <div>Respiratory digests</div>
+            <div style="flex-grow: 1 !important; -webkit-box-flex:1 !important;"></div>
+            {{-- <div style="color:#cf003d">fully booked</div> --}}
+          </div>
+          <div style="padding-bottom: 32px; ">
+              <article class="card-content text-left" style="flex-basis: 100%;margin-top: 10px;">
+                <p style="color:#015291;"><strong>Digest Author(s): Manali Effrosyni, Papiris Spyridon</strong></p>
+                <h3 class="title" style="margin-bottom:4px;">
+                <a href="professional-development/courses/calendar-sheets-in-malignant-pleural-diseases">Stability of the Blood Eosinophilic Phenotype in Stable and Exacerbated COPD</a>
+                </h3>
+                {{-- <p class="date" style="padding-bottom: 3px;"><span class="icon s7-map-marker"></span>&nbsp;Online</p> --}}
+                <p><a href="https://www.thelancet.com/journals/lanres/article/PIIS2213-2600(19)30138-9/fulltext" target="_blank">Lancet Respir Med 2019 . pii: S2213-2600(19)30138-9</a></p>
+                {{-- <figure style="position: absolute; top:40px; right:15px; bottom:auto; left: auto; z-index:1; width:100px; height:100px; overflow: hidden; border-radius: 4px;">
+                    <img style="position: absolute; height: auto; width: 200px; top: 50%; left: 50%; transform: translate(-50%,-50%);" src="https://cdn.ersnet.org/preview?node=b5472a94145dc85c715b&amp;name=img500&amp;size=500&amp;v=1350701:32118af8135260cfc850"/>
+                </figure> --}}
+              </article>
+          </div>
+          <div class="card-action clearfix" style="position: absolute; right:0;bottom: 0;"> 
+            <a href="professional-development/courses/calendar-sheets-in-malignant-pleural-diseases" class="btn btn-register">more</a>
+          </div>
+      </div><!-- End card -->
+
 
         <div class="modal-body">
         </div>
